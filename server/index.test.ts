030228aedf6676c487dde4caff8a5988116e4a06
fauxsaurus <31806063+fauxsaurus@users.noreply.github.com>
2025-10-01 14:16:37 -0500

    +ebook upload test
    The relevant route has successfully been tested.

--- server/index.test.ts
+++ server/index.test.ts
@@ -1,11 +1,38 @@
+import {exec} from 'child_process'
+import {promises} from 'fs'
+import {uploadFiles} from '../client/src/lib/request'
+import {ROUTES} from '../shared/routes'
+import {EPUB_MIMETYPE} from '../shared/mimetype'
 import {setupServer} from './setupServer'
 
+const PORT = 3000
+
 describe('Test Routes', () => {
-	const cleanup = setupServer({logger: console.log, port: 3000, state: {assetDir: ''}})
+	const cleanup = setupServer({logger: console.log, port: PORT, state: {assetDir: ''}})
 
 	it('Has a Cleanup Function', () => {
 		expect(cleanup).toBeInstanceOf(Function)
 	})
 
-	setTimeout(() => cleanup(), 0)
+	test('ebook setup', async () => {
+		await new Promise((resolve, reject) => {
+			exec('./scripts/setup-tests.sh', (error, stdout, stderr) => {
+				if (error) return reject(error)
+
+				resolve({stdout, stderr})
+			})
+		})
+
+		const buffer = await promises.readFile('./tmp/build/test.epub')
+		const blob = new Blob([buffer as BlobPart], {type: EPUB_MIMETYPE})
+
+		const url = `http://localhost:${PORT}${ROUTES.uploadEbook}`
+		const responseUploadEbook = await uploadFiles(url, {}, [['test-ebook.epub', blob]])
+		expect(responseUploadEbook).toEqual({
+			data: {assetDir: 'kindle-accessible/', files: ['index.html']},
+			errors: [],
+		})
+
+		setTimeout(() => cleanup(), 0)
+	})
 })
