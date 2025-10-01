2592a71983ea36bf923971afddb237fc076bdbd8
fauxsaurus <31806063+fauxsaurus@users.noreply.github.com>
2025-10-01 14:53:44 -0500

    simplified client/epub file routing
    The file router now checks if the requested file exists in the dist folder. If so, it serves it from there. Otherwise, it serves a file from the ebup zip archive.

--- server/routes/file-routing.ts
+++ server/routes/file-routing.ts
@@ -1,3 +1,4 @@
+import fs from 'fs'
 import mime from 'mime-types'
 import path from 'path'
 import {type IConfig, type IReq, type IRes} from './types.ts'
@@ -11,13 +12,17 @@ export const setupFileRouting = (config: Pick<IConfig, 'app' | 'state'>) => {
 			.sendFile(path.join(process.cwd(), `/client/dist/index.html`))
 	)
 
+	const clientFiles = dir2fileNames('client/dist/')
+		/** @note remove client/dist/ prefix */
+		.map(fileName => fileName.split('/').slice(2).join('/'))
+
 	/** @note Serves epub files directly from zip (and client assets). */
 	config.app.get('/*filepath', async (req: IReq, res: IRes) => {
 		const filePath = (req.params.filepath as unknown as []).join('/') || 'index.html'
 		const mimeType = mime.lookup(filePath.split('/').slice(-1)[0])
 
 		/** @note serve client files */
-		if (!filePath.includes('/') || filePath.startsWith('assets/'))
+		if (clientFiles.includes(filePath))
 			return res
 				.status(200)
 				.setHeader('Content-Type', mimeType)
@@ -29,3 +34,11 @@ export const setupFileRouting = (config: Pick<IConfig, 'app' | 'state'>) => {
 		res.status(200).setHeader('Content-Type', mimeType).send(zipEntry.getData())
 	})
 }
+
+const dir2fileNames = (dirPath: string): string[] =>
+	fs.readdirSync(dirPath).reduce((fileList, file) => {
+		const fullPath = path.join(dirPath, file)
+		const isDir = fs.statSync(fullPath).isDirectory()
+
+		return fileList.concat(isDir ? dir2fileNames(fullPath) : [fullPath])
+	}, [] as string[])
